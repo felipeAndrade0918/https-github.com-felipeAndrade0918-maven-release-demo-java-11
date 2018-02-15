021aa0d15e9f2805a8ff19c60a07b2baa05ccb6a
Felipe Andrade <felipe.andrade0918@gmail.com>
2018-02-15 20:38:28 -0200

    Commit inicial

---
+++ src/main/java/com/batuta/mavenreleasedemo/MavenReleaseDemoApplication.java
@@ -0,0 +1,12 @@
+package com.batuta.mavenreleasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReleaseDemoApplication.class, args);
+	}
+}
